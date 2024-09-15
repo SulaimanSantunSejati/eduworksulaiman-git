929e553883c9d67d38c428e011108325a56c5ead
SulaimanSantunSejati <sulaimansantun.s@gmail.com>
2024-09-16 01:06:11 +0700

    penyesuaian folder

---
+++ tugas js data types/tugasjs-datatypes.js
@@ -0,0 +1,11 @@
+ let length = 16;
+
+ let lastName ="Johnson"; 
+ 
+ const x = {           
+    firstName: "John",
+    lastName: "Doe"
+};
+console.log(typeof length);      // "number"
+console.log(typeof lastName);    // "string"
+console.log(typeof x);           // "object"
